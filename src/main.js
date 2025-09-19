d8d9b96b7b8fbcdea47d7862545bb4d64d94cc78
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-09-19 21:52:29 +0200

    feat: week8 assignment

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+var opentab = document.getElementsByClassName("tab-links");
+
+function openTab(tabname) {
+  // Remove active classes
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  // Add active classes to selected tab
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
